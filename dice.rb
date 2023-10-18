5bfe12ac770da983bd7e28894101b139bf72526c
alatif09 <146213608+alatif09@users.noreply.github.com>
2023-10-18 04:21:11 +0000

    update

--- dice.rb
+++ dice.rb
@@ -11,17 +11,10 @@ use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
-get("/") do
-  "
-  <h1>Dice Roll</h1>
+# /dice.rb
 	
-  <ul>
-    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-  </ul>
-  "
+get("/") do
+  erb(:elephant)
 end
 
 get("/zebra") do
@@ -32,47 +25,58 @@ get("/giraffe") do
   "Hopefully this shows up without having to restart the server 🤞🏾"
 end
 
+# 2d6
+
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
-	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  @rolls = []
+
+  2.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:two_six)
 end
+
+# 2d10
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
-	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  "<h1>2d10</h1>
-   <p>#{outcome}</p>"
+  @rolls = []
+
+  2.times do
+    die = rand(1..10)
+
+    @rolls.push(die)
+  end
+
+  erb(:two_ten)
 end
+
+#1d20
 
 get("/dice/1/20") do
-  first_die = rand(1..20)
-	
-  outcome = "You rolled a #{first_die}."
-	
-  "<h1>1d20</h1>
-   <p>#{outcome}</p>"
+  @rolls = []
+
+  1.times do
+    die = rand(1..20)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_twenty)
 end
 
+#5d4
+
 get("/dice/5/4") do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
-	
-  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die},and #{fifth_die}, for a total of #{sum}."
-	
-  "<h1>5d4</h1>
-   <p>#{outcome}</p>"
+  @rolls = []
+
+  5.times do
+    die = rand(1..4)
+
+    @rolls.push(die)
+  end
+
+  erb(:five_four)
 end
